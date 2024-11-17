f04f374c521e0e484ad7619d186e59a5d9201bf6
Sravya-Yeruva <yeruvasravya359@gmail.com>
2024-11-17 13:11:19 +0530

    Add files via upload

---
+++ S25-Intro to JS/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Using React Library we can create web pages much faster and much Efficient.</h1>
+      <h2>JavaScript is designed for validation. Whereas HTMl is for Creation and CSS is for Beautification.</h2>
+      <h3>JavaScript Library is nothing but JS Files.</h3>
+    </div>
+  );
+}
+
+export default App;
